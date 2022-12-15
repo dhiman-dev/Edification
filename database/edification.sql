1738ac673146007f9bf979e4b35efdb2ced61b00
Dhiman103 <71895369+dhiman-dev@users.noreply.github.com>
2022-12-16 04:07:17 +0600

    Add files via upload

--- database/edification.sql
+++ database/edification.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2022 at 02:02 PM
+-- Generation Time: Dec 15, 2022 at 11:05 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,6 +24,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `First_name` varchar(40) NOT NULL,
+  `last_name` varchar(40) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `e-mail` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`First_name`, `last_name`, `username`, `e-mail`, `password`, `status`) VALUES
+('Sunil', 'Ganguly', 'sontu', 'sganguly@gmail.com', '29e0e564dbeed95385b7c343bff1b9a3', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `all_courses`
+--
+
+CREATE TABLE `all_courses` (
+  `course_name` varchar(40) NOT NULL,
+  `instructor` varchar(45) NOT NULL,
+  `status` int(11) NOT NULL,
+  `course_id` varchar(10) NOT NULL,
+  `created_by` varchar(40) DEFAULT NULL,
+  `admin_mail` varchar(100) DEFAULT NULL,
+  `id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `all_courses`
+--
+
+INSERT INTO `all_courses` (`course_name`, `instructor`, `status`, `course_id`, `created_by`, `admin_mail`, `id`) VALUES
+('Introduction to Computer Science', 'Abdur Rahman', 1, 'ICS 2022', 'sontu', 'sganguly@gmail.com', 1),
+('Data Structure 1', 'Nurul Huda', 1, 'DSA 1', 'sontu', 'sganguly@gmail.com', 2),
+('Structured Programming Language', 'Shoeb Ahmed', 1, 'SPL 2022', 'sontu', 'sganguly@gmail.com', 3),
+('Database Management Lab', 'Mohaiminul Islam', 0, 'DBMS Lab', 'sontu', 'sganguly@gmail.com', 4),
+('Theory of Computation', 'Nabila Swarna', 0, 'TOC 2022', 'sontu', 'sganguly@gmail.com', 5),
+('Machine Learning', 'Mofijur Rahman', 1, 'ML 2022', 'sontu', 'sganguly@gmail.com', 6),
+('Computer Networking', 'Gourob Saha', 1, 'CN 2022', 'sontu', 'sganguly@gmail.com', 12),
+('Artificial Intelligence', 'Mofijur Rahman', 1, 'AI 03', 'sontu', 'sganguly@gmail.com', 13);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -37,14 +89,37 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`full_name`, `username`, `password`, `mail`, `age`, `education`) VALUES
+('Dhiman Mozumder', 'dm', '1234', 'dm@gmail.com', 23, 'tdtfghh');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `all_courses`
+--
+ALTER TABLE `all_courses`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `all_courses`
+--
+ALTER TABLE `all_courses`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
